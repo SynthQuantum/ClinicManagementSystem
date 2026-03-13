9752c40ee96a59416d30ec418793475baa2a3985
Ahmed Elsabagh <ahmed.elsabagh767@gmail.com>
2026-03-13 00:29:38 -0700

    Add Radzen Component and global json file.

--- ClinicManagementSystem.Blazor/Program.cs
+++ ClinicManagementSystem.Blazor/Program.cs
@@ -1,10 +1,12 @@
 using ClinicManagementSystem.Blazor.Components;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddRadzenComponents();
 
 var app = builder.Build();
 
